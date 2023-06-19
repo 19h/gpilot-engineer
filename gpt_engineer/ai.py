7f5548b1c3060f77111db10a1af1a380a9884b59
19h <kenan@sly.mn>
2023-06-19 12:42:42 +0200

    gopilot yolo

--- gpt_engineer/ai.py
+++ gpt_engineer/ai.py
@@ -1,15 +1,46 @@
 from __future__ import annotations
 
+import json
 import logging
+import os
+
+from typing import Optional
 
 import openai
+import requests
 
 logger = logging.getLogger(__name__)
+
+def get_github_copilot_token(api_token: str) -> Optional[str]:
+    url = "https://api.github.com/copilot_internal/v2/token"
+    headers = {
+        "Authorization": f"token {api_token}",
+        "Editor-Version": "vscode/1.79.0-insider",
+        "Editor-Plugin-Version": "copilot/1.86.92",
+        "User-Agent": "GithubCopilot/1.86.92",
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip,deflate,br",
+    }
+
+    response = requests.get(url, headers=headers)
+
+    # Check for successful status code before trying to parse as JSON
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("token")  # Return the 'token' field, if it exists
+    else:
+        return None  # or raise an exception, or handle error however you prefer
+
+
+def read_api_key() -> str:
+    # get from env (os.getenv('COPILOT_API_KEY')) or throw
+    return os.getenv("COPILOT_KEY")
 
 
 class AI:
     def __init__(self, model="gpt-4", temperature=0.1):
         self.temperature = temperature
+        self.token: str = get_github_copilot_token(read_api_key())
 
         try:
             openai.Model.retrieve(model)
@@ -44,20 +75,50 @@ class AI:
             messages += [{"role": "user", "content": prompt}]
 
         logger.debug(f"Creating a new chat completion: {messages}")
-        response = openai.ChatCompletion.create(
-            messages=messages,
-            stream=True,
-            model=self.model,
-            temperature=self.temperature,
+
+        request = self._send_request(messages)
+        response = self._parse_response(request)
+
+        messages = messages + [{"role": "assistant", "content": response}]
+
+        logger.debug(f"Chat completion finished: {messages}")
+
+        return messages
+
+    def _send_request(self, messages: list[dict[str, str]]) -> requests.Response:
+        return requests.post(
+            "https://copilot-proxy.githubusercontent.com/v1/chat/completions",
+            headers={"Authorization": "Bearer " + self.token},
+            json={
+                "model": "copilot-chat",
+                "messages": messages,
+                # "max_tokens": self.max_token_count,
+                "stream": True,
+            },
         )
 
-        chat = []
-        for chunk in response:
-            delta = chunk["choices"][0]["delta"]
-            msg = delta.get("content", "")
-            print(msg, end="")
-            chat.append(msg)
-        print()
-        messages += [{"role": "assistant", "content": "".join(chat)}]
-        logger.debug(f"Chat completion finished: {messages}")
-        return messages
+    def _parse_response(self, response: requests.Response) -> str:
+        result = ""
+
+        for chunk in response.iter_lines():
+            parsed_chunk = self._parse_stream_helper(chunk)
+
+            if parsed_chunk is not None:
+                parsed_chunk = json.loads(parsed_chunk)
+                delta = parsed_chunk["choices"][0]["delta"]
+
+                if "content" in delta:
+                    result += delta["content"]
+
+        return result
+
+    def _parse_stream_helper(self, line: bytes) -> Optional[str]:
+        if line:
+            if line.strip() == b"data: [DONE]":
+                return None
+            if line.startswith(b"data: "):
+                line = line[len(b"data: ") :]
+                return line.decode("utf-8")
+            else:
+                return None
+        return None
